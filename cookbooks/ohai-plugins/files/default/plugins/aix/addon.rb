4cd54b355debc779b8ac37e2c3f9828d9826989c
huhongbo <huhongbo@gmail.com>
2012-12-13 15:43:52 +0800

    fix aix wrong mac address

--- cookbooks/ohai-plugins/files/default/plugins/aix/addon.rb
+++ cookbooks/ohai-plugins/files/default/plugins/aix/addon.rb
@@ -30,6 +30,8 @@ popen4("lscfg -v -l ent*") do |pid, stdin, stdout, stderr|
       end
     when /Network\sAddress\.+(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})(\w{2})/
       macaddr = $1 + ":" + $2 + ":" + $3 + ":" + $4 + ":" + $5 + ":" + $6
+      f_macaddr = $1 + ":" + $2 + ":" + $3 + ":" + $4 + ":" + $5 + ":65"
+      network['interfaces'][$ifName]['addresses'].delete(f_macaddr) unless !network['interfaces'][$ifName]['addresses'].key?(f_macaddr)
       network['interfaces'][$ifName]['addresses'] = Mash.new unless network['interfaces'][$ifName]['addresses']
       network['interfaces'][$ifName]['addresses'][macaddr] = { "family" => "lladdr" }
     end
@@ -38,21 +40,21 @@ end
 
 network['interfaces'].keys.each do |ifName|
   next if ifName.match(/lo/) or ifName.match(/:/) or ifName.match(/^et\d+/)
-  ppa=ifName[/\d+/]
-  popen4("lscfg -vpl ent#{ppa}") do |pid, stdin, stdout, stderr|
-    stdin.close
-    if stdout.string.empty? then
-      popen4("entstat -d #{ifName}") do |pid, stdin, stdout, stderr|
-        member = Array.new
+      ppa=ifName[/\d+/]
+      popen4("lscfg -vpl ent#{ppa}") do |pid, stdin, stdout, stderr|
         stdin.close
-        stdout.each do |line|
-          case line
-          when /Operating\smode:\s(.+)/
+        if stdout.string.empty? then
+          popen4("entstat -d #{ifName}") do |pid, stdin, stdout, stderr|
+            member = Array.new
+            stdin.close
+            stdout.each do |line|
+              case line
+              when /Operating\smode:\s(.+)/
             network['interfaces'][ifName]['balance'] = $1
           when /ETHERNET\sSTATISTICS\s\(ent(\d+)\)/
             member << "en" + $1
             $netname = "en" + $1
-            network['interfaces'][ifName]['member'] = member.join (",")
+            network['interfaces'][ifName]['member'] = member.join(",")
           when /Device\sType:\s+(.+Link.*)/
             network['interfaces'][ifName]['name'] = $1
           when /Link\sStatus\s*:\s*(.+)/
@@ -84,28 +86,28 @@ popen4("lscfg -v -l fcs*") do  |pid, stdin, stdout, stderr|
         stdin.close
         stdout.each do |line|
           if line.match(/attach\s+none/) then
-            storage[:interfaces][$fcid][:status] = "DOWN"
-            break
-          else
-            storage[:interfaces][$fcid][:status] = "UP"
-            if File.executable?("/usr/sbin/fcstat") then
-              popen4("fcstat #{$fcid} | grep running") do |pid, stdin, stdout, stderr|
-                stdin.close
-                stdout.each do |line|
-                  if line.match(/^Port\s+Speed\s+\(running\):\s+(\d)/i) then
-                    storage[:interfaces][$fcid][:speed] = $1 + "Gb"
-                  else
-                    storage[:interfaces][$fcid][:speed] = "UNKNOWN"
+                storage[:interfaces][$fcid][:status] = "DOWN"
+                break
+              else
+                storage[:interfaces][$fcid][:status] = "UP"
+                if File.executable?("/usr/sbin/fcstat") then
+                  popen4("fcstat #{$fcid} | grep running") do |pid, stdin, stdout, stderr|
+                    stdin.close
+                    stdout.each do |line|
+                      if line.match(/^Port\s+Speed\s+\(running\):\s+(\d)/i) then
+                        storage[:interfaces][$fcid][:speed] = $1 + "Gb"
+                      else
+                        storage[:interfaces][$fcid][:speed] = "UNKNOWN"
+                      end
+                    end
                   end
+                else
+                  storage[:interfaces][$fcid][:speed] = "UNKNOWN"
                 end
               end
-            else
-              storage[:interfaces][$fcid][:speed] = "UNKNOWN"
             end
           end
-        end
-      end
-    when /Network\s+Address\.+(..)(..)(..)(..)(..)(..)(..)(..)/
+          when /Network\s+Address\.+(..)(..)(..)(..)(..)(..)(..)(..)/
       storage[:interfaces][$fcid][:wwn] = $1+":"+$2+":"+$3+":"+$4+":"+$5+":"+$6+":"+$7+":"+$8
     end
   end
