d224bd0d4647aacafadb825f3a1a2e3c7d4dee1f
dn365 <duning3650@gmail.com>
2013-04-10 10:45:02 +0800

    添加ntp server and ntp client 配置

---
+++ cookbooks/someset_system/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for someset_system
+
+This file is used to list changes made in each version of someset_system.
+
+## 0.1.0:
+
+* Initial release of someset_system
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ cookbooks/someset_system/recipes/ntp.rb
@@ -0,0 +1,41 @@
+# set ntp conf
+
+file = node['ntp']['file']
+
+ntp_group = value_for_platform(
+{
+  "ubuntu" => {"default"=>"ubuntu_ntp.conf.erb"},
+  "suse" => {"default"=>"suse_ntp.conf.erb"},
+  "aix" => {"default"=>"aix_ntp.conf.erb"},
+  "hpux" => {"default"=>"hpux_ntp.conf.erb"}
+})
+
+puts ntp_group
+
+template file do
+  source "ntp/#{ntp_group}"
+  variables(:def_ntp => node['ntp']['server'])
+  #notifies :restart, "service[ntp]", :delayed
+end
+
+
+#server
+
+service "ntp" do
+  case node['os']
+  when "hpux"
+    service_name "xntpd"
+    provider Chef::Provider::Service::Hpux
+  when "aix"
+    service_name "xntpd"
+    start_command "startsrc -s xntpd"
+    stop_command "stopsrc -s xntpd"
+    restart_command "stopsrc -s xntpd && sleep 2 && startsrc -s xntpd"
+    status_command "ps exw | grep xntpd | grep -v grep | awk '{ print $1 }'"
+  end
+  supports :restart => true, :status => true
+  action :start
+  subscribes :restart, resources("template[#{file}]"), :delayed
+end
+
+  
