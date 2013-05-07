0dd930cf4fc69efea31ec46bfcafbb9991e832af
dn365 <duning3650@gmail.com>
2013-05-07 09:46:50 +0800

    修复文件细节错误: 更早文件版本号，下发文件后缀错误,添加mcollective agent plugin

--- cookbooks-11.40/mcollective/attributes/default.rb
+++ cookbooks-11.40/mcollective/attributes/default.rb
@@ -12,7 +12,7 @@ default["mcollective"]["stomp"]["password"] = "Passw0rd"
 default["mcollective"]["version"] = "2.2.3"
 
 # mcollective plugins path
-default["mcollective"]["config_dir"] = "/etc/mcollective/"
+default["mcollective"]["config_dir"] = "/etc/mcollective"
 default["mcollective"]["libdir"] = "#{node["mcollective"]["config_dir"]}/mcollective-plugins"
 
 default["mcollective"]["log_dir"] = "/var/log"
